e30e570fb7345dab0a00acec253c7d3249158174
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-19 11:50:09 +0300

    Defined a sample endpoint for the inventory microservice

---
+++ inventory/app/main.py
@@ -0,0 +1,24 @@
+"""The main script for the app"""
+from fastapi import FastAPI
+
+
+app = FastAPI(
+    title="Inventory Microservice",
+    description="The microservice that deals with the inventory system"
+)
+
+
+@app.get(
+    "/",
+    name="Root",
+    description="The root endpoint of the inventory microservice",
+    tags=['Inventory Root'],
+    response_model=dict[str, str]
+)
+async def root() -> dict[str, str]:
+    """The root endpoint of the inventory microservice
+
+    Returns:
+        dict[str, str]: A simple message
+    """
+    return {"Message": "The inventory microservice is running"}
