4c050f2167256e53ac213bc76f368ce8ec90072c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-19 15:09:14 +0300

    Defined the endpoint to create a request

--- payment/routes/orders_route.py
+++ payment/routes/orders_route.py
@@ -1,8 +1,30 @@
 """The orders route file"""
-from fastapi import APIRouter, status
+import requests
+from fastapi import APIRouter, Request, status
 
 
 orders_router = APIRouter(
-    prefix="/orders", 
+    prefix="/orders",
     tags=["Orders"]
-    )
+)
+
+
+@orders_router.post(
+    "/",
+    name="Make an Order",
+    description="The endpoint to make an order",
+    status_code=status.HTTP_201_CREATED
+)
+async def create_order_endpoint(_request: Request):
+    """The endpoint to make an order
+
+    Args:
+        _request (Request): No idea
+
+    Returns:
+        _type_: Something
+    """
+    _body = await _request.json()
+    _req = requests.get(f'http://localhost:8000/products/%s' % _body['id'], timeout=10000)
+
+    return _req
