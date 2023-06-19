7ecbb563d8be0c7849d8cf26a2730180ca094bdc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-19 16:01:28 +0300

    Created a function to update the order state and call it when the order is created

--- payment/routes/orders_route.py
+++ payment/routes/orders_route.py
@@ -25,18 +25,30 @@ async def create_order_endpoint(_request: Request):
         _type_: Something
     """
     _body = await _request.json()
-    _req = requests.get('http://localhost:8100/products/%s' % _body['id'], timeout=10000)
+    _req = requests.get('http://localhost:8100/products/%s' %
+                        _body['id'], timeout=10000)
     _product = _req.json()
-    
+
     _order = Order(
-        product_id = _body['id'],
-        price = _product['price'],
-        fee = _product['price'] * 0.2,
-        total = _product['price'] * 1.2,
-        quantity = _body['quantity'],
+        product_id=_body['id'],
+        price=_product['price'],
+        fee=_product['price'] * 0.2,
+        total=_product['price'] * 1.2,
+        quantity=_body['quantity'],
         status='pending'
     )
-    
+
     _order.save()
 
+    order_completed(_order)
+
     return _order
+
+
+def order_completed(_order: Order):
+    """A function called when the order is completed
+
+    Args:
+        _order (Order): The order
+    """
+    _order.status = 'completed'
