cfef1c2fb1c341521eb7fa58121568850e487ea0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-19 12:34:44 +0300

    Defined the origins for the middleware

--- inventory/app/main.py
+++ inventory/app/main.py
@@ -8,9 +8,14 @@ app = FastAPI(
     description="The microservice that deals with the inventory system"
 )
 
+origins=[
+    'http://localhost:5173',
+    'https://localhost:5173'
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
