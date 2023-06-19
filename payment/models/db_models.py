f23ce2d983abcd33743b670af728ad805dc06e6a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-19 14:52:41 +0300

    CReated the model for the Orders

---
+++ payment/models/db_models.py
@@ -0,0 +1,16 @@
+"""The models for the Order data"""
+from redis_om import HashModel
+
+
+class Order(HashModel):
+    """The model for the orders
+
+    Args:
+        HashModel (Redis_om): The base class for the Order class
+    """
+    product_id: str
+    price: float
+    fee: float
+    total: float
+    quantity: int
+    status: str
