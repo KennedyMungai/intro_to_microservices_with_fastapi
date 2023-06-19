82a627bc3ee76ca3d21f2f4e6dc8cfac0b646b9d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-19 14:35:30 +0300

    Defined the database logic for the microservice

---
+++ payment/database/db.py
@@ -0,0 +1,15 @@
+"""The database connection script"""
+import os
+from dotenv import load_dotenv, find_dotenv
+from redis_om import get_redis_connection
+
+load_dotenv(find_dotenv())
+
+REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD')
+
+redis=get_redis_connection(
+    host='redis-10746.c8.us-east-1-2.ec2.cloud.redislabs.com',
+    port=10746,
+    password=REDIS_PASSWORD,
+    decode_responses=True
+)
