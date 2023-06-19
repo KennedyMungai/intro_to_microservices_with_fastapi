e2fd73701ac2d523e51b22dcbc1cbe687fcc1750
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-19 13:01:45 +0300

    Created the ProductDisplay schema

--- inventory/schemas/product_schema.py
+++ inventory/schemas/product_schema.py
@@ -1,1 +1,18 @@
 """The schemas for the Products"""
+from pydantic import BaseModel
+
+
+class ProductDisplay(BaseModel):
+    """The product Display Schema
+
+    Args:
+        BaseModel (Pydantic Model): The base model for all models
+    """
+    pk: str
+    name: str
+    price: float
+    quantity: int
+
+    class Config:
+        """The config subclass for the ProductDisplay Schema"""
+        orm_mode = True
