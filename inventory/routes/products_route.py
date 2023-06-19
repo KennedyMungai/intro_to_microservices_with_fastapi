52b7dd51bc62c3847eeef60b59db9d8e24f01262
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-19 12:55:10 +0300

    Defined the route to create a product

--- inventory/routes/products_route.py
+++ inventory/routes/products_route.py
@@ -1,5 +1,5 @@
 """The products routing logic"""
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
 from models.db_models import Product
 
@@ -22,3 +22,21 @@ async def get_all_products_endpoint():
         _type_: _description_
     """
     return Product.all_pks()
+
+
+@products_router.post(
+    "/",
+    name='Create Product',
+    description="Creates a new product",
+    status_code=status.HTTP_201_CREATED
+)
+async def create_product_endpoint(product: Product):
+    """The endpoint to create a product
+
+    Args:
+        product (Product): The data needed to create the product
+
+    Returns:
+        _type_: _description_
+    """
+    return product.save()
